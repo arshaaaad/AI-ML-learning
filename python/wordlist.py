e435cd94dc43c4d0dfc4bd9e9b902bc0166987ea
Arshad <arshad7019424@gmail.com>
2026-04-16 21:21:38 +0530

    clean repo

--- python/wordlist.py
+++
@@ -1,6 +0,0 @@
-words = ("apple", "mango", "banana", "orange", "grape", "melon", "guava",
-     "papaya", "pear", "plum", "kiwi", "pineapple", "pomegranate",
-    "blueberry", "blackberry", "strawberry", "avocado", "lychee",
-    "apricot", "fig", "dates", "peach", "olive", "watermelon",
-    "dragonfruit", "passionfruit", "tangerine", "cantaloupe",
-    "mandarin", "persimmon", "rambutan", "mulberry", "jackfruit" )
